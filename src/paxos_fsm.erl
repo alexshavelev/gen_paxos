5cae799f10201adeb45fc000de5823b6a65cf15b
Alexander Shavelev <alexshavelev44@gmail.com>
2016-07-26 09:53:20 +0300

    add sending message in init

--- src/paxos_fsm.erl
+++ src/paxos_fsm.erl
@@ -95,6 +95,8 @@ io:format("Line: ~p~n", [?LINE]),
     io:format("~p ~p: ~p~n", [?MODULE, started, InitStateData]),
     %% starting paxos_fsm...
     process_flag(trap_exit, true),
+    Result = broadcast( InitStateData#state.others, InitStateData#state.subject, {prepare, {InitStateData#state.subject, InitStateData#state.n, InitStateData#state.value, node()}} ),
+    io:format( "broadcast: ~p. ~n", [Result]),
     {ok,
      nil,  %% initial statename
      InitStateData,     %%{{S, InitN, V},{All, Quorum, 0, Others, InitN}, Misc }, %% initial state data
